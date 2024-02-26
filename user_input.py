273604a50657c3a7dea8a59ca73db2310564b93a
colspan <collin@collindigm.com>
2024-02-26 11:53:58 +0300

    user_input program

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name = input("Enter name: ");
+age = input("Enter age: ");
+location = input("Enter location: ");
+
+print("Hello {}, you are {} years old and live in {}.".format(name, age, location))
